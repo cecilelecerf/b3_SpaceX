37868af9379908d1664a635ad5c02f9fd9100ec6
Cécile Lecerf <cecel@Cecoule-Macbook.local>
2023-10-10 09:13:07 +0200

    create folder and file for controllers and routes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const postRoute = require('./routes/postRoute');
+postRoute(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/routes/postRoute.js
@@ -0,0 +1,6 @@
+module.exports = (server) => {
+    const postController = require('../controllers/postController');
+    server.route('posts')
+    .get(postController.listenAllPosts)
+    .post(postController.createAPost);
+}
